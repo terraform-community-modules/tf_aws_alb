e1769a8be8c70a1bf8273c5e642a01c4f2233e4a
Brandon O'Connor <boconnor@tableau.com>
2017-03-08 18:13:09 -0800

    fixed up the variables example

--- example/variables.tf
+++ example/variables.tf
@@ -1,7 +1,3 @@
-variable "aws_access_key" {}
-
-variable "aws_secret_key" {}
-
 variable "aws_region" {
   default = "us-west-2"
 }
@@ -10,7 +6,7 @@ variable "alb_name" {
   default = "example_alb"
 }
 
-variable "alb_security_groups" {
+variable "security_group_id_list" {
   default = ["sg-edcd9784", "sg-edcd9785"]
 }
 
@@ -18,26 +14,30 @@ variable "certificate_arn" {
   default = "arn:aws:iam::123456789012:server-certificate/ProdServerCert"
 }
 
-variable "subnets" {
+variable "public_subnet_ids" {
   default = ["subnet-1a2b3c4d", "subnet-1a2b3c4e", "subnet-1a2b3c4f"]
 }
 
-variable "backend_port" {
+variable "instance_port" {
   default = "8080"
 }
 
-variable "backend_protocol" {
-  default = "http"
+variable "instance_protocol" {
+  default = "HTTPS"
 }
 
 variable "health_check_target" {
   default = "HTTPS:443/health"
 }
 
-variable "log_bucket" {
+variable "log_bucket_name" {
   default = "my_log_bucket"
 }
 
 variable "log_prefix" {
   default = "example_alb"
+}
+
+variable "vpc_id" {
+  default = "vpc-12345678"
 }
